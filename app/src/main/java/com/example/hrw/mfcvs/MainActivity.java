9bdded907fe80a398faa451a4057b1268cf8111a
Heiru Wu <wuheiru.5203@gmail.com>
2014-07-29 15:55:04 +0800

    front camera recording and encoding with h.264, AAC, save in sdcard

--- app/src/main/java/com/example/hrw/mfcvs/MainActivity.java
+++ app/src/main/java/com/example/hrw/mfcvs/MainActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.hardware.Camera;
+import android.media.CamcorderProfile;
+import android.media.CameraProfile;
+import android.media.MediaCodec;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,6 +18,7 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.Toast;
 
@@ -57,16 +62,21 @@ public class MainActivity extends Activity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment implements SurfaceHolder.Callback {
+    public static class PlaceholderFragment extends Fragment implements SurfaceHolder.Callback, View.OnClickListener {
         private Camera mCamera;
         SurfaceView previewSurfaceView;
         SurfaceHolder previewSurfaceHolder;
+        VideoCodec videoCodec;
+        Button record;
+        boolean isNotRec = true;
 
         @Override
         public void onActivityCreated(Bundle savedInstanceState) {
             super.onActivityCreated(savedInstanceState);
 //          Runtime.getRuntime().exec("su");
             mCamera = getCameraInstance();
+            record = (Button)getView().findViewById(R.id.record);
+            record.setOnClickListener(this);
             if(mCamera == null){
                 Toast.makeText(getActivity(),
                         "Fail to get Camera",
@@ -75,6 +85,7 @@ public class MainActivity extends Activity {
             previewSurfaceView = (SurfaceView)getView().findViewById(R.id.surfaceview);
             previewSurfaceHolder = previewSurfaceView.getHolder();
             previewSurfaceHolder.addCallback(this);
+            videoCodec = new VideoCodec("test",mCamera);
         }
 
         public Camera getCameraInstance() {
@@ -99,6 +110,10 @@ public class MainActivity extends Activity {
                     try {
                         cam = Camera.open(camIdx);
                         Log.w("Camera","No:"+String.valueOf(camIdx)+" get");
+                        Camera.Parameters param = cam.getParameters();
+                        param.set( "cam_mode", 1 );
+                        param.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO);
+                        cam.setParameters( param );
                     } catch (RuntimeException e) {
                         Log.e("Your_TAG", "Camera failed to open: " + e.getLocalizedMessage());
                     }
@@ -106,8 +121,6 @@ public class MainActivity extends Activity {
             }
             return cam;
         }
-
-
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -135,6 +148,23 @@ public class MainActivity extends Activity {
         @Override
         public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
             mCamera.release();
+        }
+
+        @Override
+        public void onClick(View view) {
+            if(isNotRec) {
+                Toast.makeText(getActivity(),"Record Start",Toast.LENGTH_SHORT).show();
+                isNotRec = false;
+                try {
+                    videoCodec.startEncoding();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }else{
+                Toast.makeText(getActivity(), "Record Stop", Toast.LENGTH_SHORT).show();
+                isNotRec = true;
+                videoCodec.stopEncoding();
+            }
         }
     }
 }
--- app/src/main/java/com/example/hrw/mfcvs/VideoCodec.java
+++ app/src/main/java/com/example/hrw/mfcvs/VideoCodec.java
@@ -1,7 +1,47 @@
 package com.example.hrw.mfcvs;
 
+import android.hardware.Camera;
+import android.media.MediaRecorder;
+import android.widget.Toast;
+
+import java.io.IOException;
+
 /**
  * Created by hrw on 2014/7/25.
  */
 public class VideoCodec {
+    MediaRecorder mrec;
+    Camera mCamera;
+    String filename;
+
+
+    public VideoCodec(String filename,Camera mCamera){
+        mrec = new MediaRecorder();
+        this.filename = filename;
+        this.mCamera = mCamera;
+    }
+
+    public void startEncoding() throws IOException {
+        mrec.setCamera(mCamera);
+        mCamera.unlock();
+        mrec.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+        mrec.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mrec.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        mrec.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+        mrec.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        mrec.setVideoSize(720,480);
+        mrec.setVideoEncodingBitRate(30000000);
+        mrec.setAudioEncodingBitRate(196608);
+        mrec.setOrientationHint(270);
+        mrec.setVideoFrameRate(30);
+        mrec.setOutputFile("/sdcard/"+filename+".mp4");
+        mrec.prepare();
+        mrec.start();
+    }
+
+    public void stopEncoding(){
+        mrec.stop();
+        mrec.reset();
+        mrec.release();
+    }
 }
